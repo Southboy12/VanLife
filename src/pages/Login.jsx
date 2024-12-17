b217fedf86f872d68d17530eaf754e31da06c582
southboy12 <menkachi85@gmail.com>
2024-12-17 11:35:01 +0100

    Implemented the login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,35 @@
 import React from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useLocation } from "react-router-dom"
+import { loginUser } from "../api"
+
+/**
+ * Challenge: read up on the useNavigate hook from the
+ * docs and implement it in the VanLife app. When the user
+ * successfully logs in, they should be redirected to the 
+ * /host route.
+ */
 
 export default function Login() {
     const [loginFormData, setLoginFormData] = React.useState({ email: "", password: "" })
+    const [status, setStatus] = React.useState("idle")
+    const [error, setError] = React.useState(null)
+
     const location = useLocation()
-    
 
     function handleSubmit(e) {
         e.preventDefault()
-        console.log(loginFormData)
+        setStatus("submitting")
+        loginUser(loginFormData)
+            .then(data => {
+                console.log(data)
+                setError(null)
+            })
+            .catch(err => {
+                setError(err)
+            })
+            .finally(() => {
+                setStatus("idle")
+            })
     }
 
     function handleChange(e) {
@@ -21,8 +42,16 @@ export default function Login() {
 
     return (
         <div className="login-container">
-            {location.state?.message && <h3>{location.state.message}</h3>}
+            {
+                location.state ?.message &&
+                    <h3 className="login-error">{location.state.message}</h3>
+            }
             <h1>Sign in to your account</h1>
+            {
+                error ?.message &&
+                    <h3 className="login-error">{error.message}</h3>
+            }
+
             <form onSubmit={handleSubmit} className="login-form">
                 <input
                     name="email"
@@ -38,7 +67,14 @@ export default function Login() {
                     placeholder="Password"
                     value={loginFormData.password}
                 />
-                <button>Log in</button>
+                <button
+                    disabled={status === "submitting"}
+                >
+                    {status === "submitting"
+                        ? "Logging in..."
+                        : "Log in"
+                    }
+                </button>
             </form>
         </div>
     )
